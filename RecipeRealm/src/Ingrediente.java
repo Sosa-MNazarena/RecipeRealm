b047c18d1a86946cc810bc195aaaca74db5ae3b2
Constanza Delfino <111506427+cdelfino@users.noreply.github.com>
2024-04-27 16:26:29 -0300

    Editar Ingrediente y Resena

--- RecipeRealm/src/Ingrediente.java
+++ RecipeRealm/src/Ingrediente.java
@@ -1,4 +1,27 @@
 
 public class Ingrediente {
+	private String nombre;
+	private double cantidad;
+
+	public Ingrediente(String nombre, double cantidad, String unidad) {
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public double getCantidad() {
+		return cantidad;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
 
 }
--- RecipeRealm/src/Resena.java
+++ RecipeRealm/src/Resena.java
@@ -1,4 +1,54 @@
+import java.util.Date;
 
 public class Resena {
+	private String comentario;
+	private int estrellas;
+	private Aficionado autor;
+	private Date fecha;
 
+	// Constructor
+	public Resena(String comentario, int estrellas, Aficionado autor, Date fecha) {
+		this.comentario = comentario;
+		this.autor = autor;
+		this.fecha = fecha;
+
+		// Para que las estrellas esten entre 1 y 5
+		if (estrellas < 1) {
+			this.estrellas = 1; 
+		} else if (estrellas > 5) {
+			this.estrellas = 5; 
+		} else {
+			this.estrellas = estrellas; 
+		}
+	}
+
+	public String getComentario() {
+		return comentario;
+	}
+
+	public int getEstrellas() {
+		return estrellas;
+	}
+
+	public Aficionado getAutor() {
+		return autor;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+
+	 public void setEstrellas(int estrellas) {
+	        if (estrellas < 1) {
+	            this.estrellas = 1; 
+	        } else if (estrellas > 5) {
+	            this.estrellas = 5; 
+	        } else {
+	            this.estrellas = estrellas;
+	        }
+	    }
 }
