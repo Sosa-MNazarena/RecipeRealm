200f54db0f43361867e9984e47a3fc8e295a5645
Constanza Delfino <111506427+cdelfino@users.noreply.github.com>
2024-04-27 15:50:15 -0300

    create menu in main

--- RecipeRealm/src/Main.java
+++ RecipeRealm/src/Main.java
@@ -1,11 +1,29 @@
+import javax.swing.JOptionPane;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Main
-		//Prueba de cambios
-		
+		int opcion = Integer.parseInt(JOptionPane.showInputDialog("Ingrese una opción (1, 2, o 3):"));
+
+		switch (opcion) {
+		case 1:
+			JOptionPane.showMessageDialog(null, "Menu 1");
+			break;
+
+		case 2:
+			JOptionPane.showMessageDialog(null, "Menu 2");
+			break;
+
+		case 3:
+			JOptionPane.showMessageDialog(null, "Menu 3");
+			break;
+
+		default:
+			JOptionPane.showMessageDialog(null, "Ingrese una opción valida");
+			break;
+		}
 	}
 
 }
