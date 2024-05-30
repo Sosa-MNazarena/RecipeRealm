cb93715a88aeee1700d434e960d651b3c5a36d26
Sosa-Nazarena <mariana.sosa@davinci.edu.ar>
2024-05-30 14:37:46 -0300

    feat: testeo curso

--- RecipeRealm/src/Tests/TestCursos.java
+++ RecipeRealm/src/Tests/TestCursos.java
@@ -20,43 +20,46 @@ public class TestCursos {
 	@Test
     public void testPublicarCurso_Valido() {
 		CursoControlador controlador=new CursoControlador();
-        Perfil perfilVerificado = new Perfil(10, "Nazarena", "nachi", "naza@gmail.com", "contra123", 
-                                             "descripción larga del instructor que cumple con el mínimo requerido",
+        Perfil perfilVerificado = new Perfil(10, "Nazarena", "nachi", "naza@gmail.com", "Contra_123", 
+        		"me gusta cocinar y pasar tiempo con mi perro, preparando comidas argentinas",
                                              true);
         
-        Cursos cursoValido = new Cursos(5, "Curso de Cocina Avanzada", perfilVerificado, 
-                                        "Cocina Central", LocalDate.now().plusDays(1), 20, 100.0, LocalTime.of(15,00,00));
+        Cursos cursoValido = new Cursos(5, "Curso de Cocina Avanzada", 
+                                        perfilVerificado, "San Martin 2309", LocalDate.now().plusDays(1), 20, 100.0, LocalTime.of(15,00,00));
 
 
-        assertEquals(controlador.addCurso(cursoValido), true);
+        assertEquals(true, controlador.addCurso(cursoValido, perfilVerificado));
     }
 	
 	@Test
     public void testPublicarCurso_NoVerificado() {
 		CursoControlador controlador=new CursoControlador();
-        Perfil perfilNoVerificado = new Perfil(10, "Nazarena", "nachi", "naza@gmail.com", "contra123", 
-                "descripción larga del instructor que cumple con el mínimo requerido",
+        Perfil perfilNoVerificado = new Perfil(10, "Nazarena", "nachi", "naza@gmail.com", "Contra_123", 
+                "me gusta cocinar y pasar tiempo con mi perro, preparando comidas argentinas",
                                                false);
         
         Cursos cursoNoVerificado = new Cursos(4, "Curso de Cocina Avanzada", perfilNoVerificado, 
-                                              "Cocina Central", LocalDate.now().plusDays(1), 20, 100.0, LocalTime.now());
+        		"San Martin 2309", LocalDate.now().plusDays(1), 20, 100.0, LocalTime.now());
 
-        assertEquals(controlador.addCurso(cursoNoVerificado), false);
+        assertEquals(controlador.addCurso(cursoNoVerificado, perfilNoVerificado), false);
     }
 	
 	
-	
 	@Test
-    public void testPublicarCurso_CupoExcedido() {
+    public void testPublicarCurso_TituloCorto() {
 		CursoControlador controlador=new CursoControlador();
-        Perfil perfilVerificado = new Perfil(7, "Laura", "laurita", "laura@gmail.com", "contra000", 
-        		"descripción larga del instructor que cumple con el mínimo requerido",
+        Perfil perfilInscripcion = new Perfil(7, "Laura", "laurita", "laura@gmail.com", "contra000", 
+        		"me gusta cocinar y pasar tiempo con mi perro, preparando comidas argentinas",
                                              true);
 
-        Cursos cursoCupoExcedido = new Cursos(5, "Curso de Cocina Avanzada", perfilVerificado, 
-	                                              "Cocina Central", LocalDate.now().plusDays(1), 101, 100.0, LocalTime.now());
+        Cursos tituloCorto = new Cursos(5, "Cocinando", perfilInscripcion, 
+	                                              "Lavalle 2743", LocalDate.now().plusDays(1), 20, 100.0, LocalTime.now());
 
-	    assertEquals(controlador.addCurso(cursoCupoExcedido), false);
+	    assertEquals(controlador.addCurso(tituloCorto, perfilInscripcion), false);
 	 }
+	
+	
+	
+	
 
 }
