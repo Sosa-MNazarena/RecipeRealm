9eed1ff6621bfb224c3936d2d70a9c06216336e5
SofiLeonar <sleonarholz@gmail.com>
2024-05-30 00:27:15 -0300

    Refactor: clase "Perfil"

--- RecipeRealm/src/Modelos/Perfil.java
+++ RecipeRealm/src/Modelos/Perfil.java
@@ -9,94 +9,80 @@ public class Perfil {
     private String contrasena;
     private String descripcion;
     private boolean verificado;
-
-
-    public Perfil(int idUsuario, String nombre, String pseudonimo, String correo, String contrasena, String descripcion, boolean verificado) {
-        this.setIdUsuario(idUsuario);
-        this.setNombre(nombre);
-        this.setPseudonimo(pseudonimo);
-        this.setCorreo(correo);
-        this.setContrasena(contrasena);
-        this.setDescripcion(descripcion);
-        this.setVerificado(verificado);
-    }
-
+    
+	public Perfil(int idUsuario, String nombre, String pseudonimo, String correo, String contrasena, String descripcion,
+			boolean verificado) {
+		super();
+		this.idUsuario = idUsuario;
+		this.nombre = nombre;
+		this.pseudonimo = pseudonimo;
+		this.correo = correo;
+		this.contrasena = contrasena;
+		this.descripcion = descripcion;
+		this.verificado = verificado;
+	}
 
 	public int getIdUsuario() {
 		return idUsuario;
 	}
 
-
 	public void setIdUsuario(int idUsuario) {
 		this.idUsuario = idUsuario;
 	}
-
 
 	public String getNombre() {
 		return nombre;
 	}
 
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
 
 	public String getPseudonimo() {
 		return pseudonimo;
 	}
 
-
 	public void setPseudonimo(String pseudonimo) {
 		this.pseudonimo = pseudonimo;
 	}
-
 
 	public String getCorreo() {
 		return correo;
 	}
 
-
 	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
-
 
 	public String getContrasena() {
 		return contrasena;
 	}
 
-
 	public void setContrasena(String contrasena) {
 		this.contrasena = contrasena;
 	}
-
 
 	public String getDescripcion() {
 		return descripcion;
 	}
 
-
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-
 
 	public boolean isVerificado() {
 		return verificado;
 	}
 
-
 	public void setVerificado(boolean verificado) {
 		this.verificado = verificado;
 	}
 
-
-	public boolean getisVerificado() {
-		// TODO Auto-generated method stub
-		return false;
+	@Override
+	public String toString() {
+		return "Perfil [idUsuario=" + idUsuario + ", nombre=" + nombre + ", pseudonimo=" + pseudonimo + ", correo="
+				+ correo + ", contrasena=" + contrasena + ", descripcion=" + descripcion + ", verificado=" + verificado
+				+ "]";
 	}
-
-
-
+    
 }
