2ff77c2e5179ab6786f02ab89b4509f7774c1ff9
SofiLeonar <sleonarholz@gmail.com>
2024-05-29 21:06:38 -0300

    chef

--- RecipeRealm/src/Controladores/ChefControlador.java
+++ RecipeRealm/src/Controladores/ChefControlador.java
@@ -1,5 +1,12 @@
 package Controladores;
 
+import java.sql.Connection;
+
 public class ChefControlador {
+	private final Connection connection;
+
+    public ChefControlador() {
+        this.connection = DatabaseConnection.getInstance().getConnection();
+    }
 
 }
