a198a4123754b34cce06c4341634b5e3a5b1e509
SofiLeonar <sleonarholz@gmail.com>
2024-06-27 00:38:56 -0300

    feat: estilo "editar cuso"

--- RecipeRealm/src/Vistas/PantallaEditarCurso.java
+++ RecipeRealm/src/Vistas/PantallaEditarCurso.java
@@ -34,18 +34,22 @@ public class PantallaEditarCurso extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 800, 600);
         contentPane = new JPanel();
-        contentPane.setBackground(new Color(242, 242, 242));
+        contentPane.setBackground(Color.DARK_GRAY);
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(contentPane);
         contentPane.setLayout(null);
 
         JLabel lblEditarCursos = new JLabel("Editar Cursos");
-        lblEditarCursos.setFont(new Font("Leelawadee UI Semilight", Font.PLAIN, 26));
-        lblEditarCursos.setBounds(300, 30, 200, 34);
+        lblEditarCursos.setHorizontalAlignment(SwingConstants.CENTER);
+        lblEditarCursos.setForeground(Color.WHITE);
+        lblEditarCursos.setFont(new Font("Lucida Console", Font.BOLD, 26));
+        lblEditarCursos.setBounds(0, 20, 786, 44);
+        lblEditarCursos.setIcon(new ImageIcon(getClass().getClassLoader().getResource("imagenes/libroIconMini.png")));
         contentPane.add(lblEditarCursos);
 
         JLabel lblTitulo = new JLabel("Título:");
-        lblTitulo.setFont(new Font("Leelawadee UI Semilight", Font.PLAIN, 16));
+        lblTitulo.setForeground(Color.WHITE);
+        lblTitulo.setFont(new Font("Lucida Console", Font.PLAIN, 12));
         lblTitulo.setBounds(104, 98, 100, 20);
         contentPane.add(lblTitulo);
 
@@ -55,7 +59,8 @@ public class PantallaEditarCurso extends JFrame {
         txtTitulo.setColumns(10);
 
         JLabel lblLugar = new JLabel("Lugar:");
-        lblLugar.setFont(new Font("Leelawadee UI Semilight", Font.PLAIN, 16));
+        lblLugar.setForeground(Color.WHITE);
+        lblLugar.setFont(new Font("Lucida Console", Font.PLAIN, 12));
         lblLugar.setBounds(104, 148, 100, 20);
         contentPane.add(lblLugar);
 
@@ -65,7 +70,8 @@ public class PantallaEditarCurso extends JFrame {
         txtLugar.setColumns(10);
 
         JLabel lblPrecio = new JLabel("Precio:");
-        lblPrecio.setFont(new Font("Leelawadee UI Semilight", Font.PLAIN, 16));
+        lblPrecio.setForeground(Color.WHITE);
+        lblPrecio.setFont(new Font("Lucida Console", Font.PLAIN, 12));
         lblPrecio.setBounds(104, 198, 100, 20);
         contentPane.add(lblPrecio);
 
@@ -75,7 +81,8 @@ public class PantallaEditarCurso extends JFrame {
         txtPrecio.setColumns(10);
 
         JLabel lblCupos = new JLabel("Cupos Disponibles:");
-        lblCupos.setFont(new Font("Leelawadee UI Semilight", Font.PLAIN, 16));
+        lblCupos.setForeground(Color.WHITE);
+        lblCupos.setFont(new Font("Lucida Console", Font.PLAIN, 12));
         lblCupos.setBounds(104, 248, 150, 20);
         contentPane.add(lblCupos);
 
@@ -85,18 +92,31 @@ public class PantallaEditarCurso extends JFrame {
         txtCupos.setColumns(10);
 
         JLabel lblDia = new JLabel("Día:");
-        lblDia.setFont(new Font("Leelawadee UI Semilight", Font.PLAIN, 16));
+        lblDia.setForeground(Color.WHITE);
+        lblDia.setFont(new Font("Lucida Console", Font.PLAIN, 12));
         lblDia.setBounds(104, 298, 100, 20);
         contentPane.add(lblDia);
 
         dateChooser = new JDateChooser();
-        dateChooser.setBounds(214, 296, 460, 30);
+        dateChooser.setBounds(214, 299, 150, 37);
         dateChooser.setDate(Date.from(curso.getDia().atStartOfDay(ZoneId.systemDefault()).toInstant())); 
         contentPane.add(dateChooser);
 
+        Box horizontalBox1 = Box.createHorizontalBox();
+		horizontalBox1.setBounds(264, 347, 100, 44);
+		contentPane.add(horizontalBox1);
+		
+		JSpinner inputHora = new JSpinner();
+		horizontalBox1.add(inputHora);
+		inputHora.setModel(new SpinnerNumberModel(0, 0, 24, 1));
+		
+		JSpinner inputMinuto = new JSpinner();
+		horizontalBox1.add(inputMinuto);
+		inputMinuto.setModel(new SpinnerNumberModel(0, 0, 59, 1));
+
         JButton btnGuardar = new JButton("Guardar");
-        btnGuardar.setBackground(new Color(255, 255, 255));
-        btnGuardar.setFont(new Font("Leelawadee UI Semilight", Font.PLAIN, 16));
+        btnGuardar.setBackground(new Color(255, 153, 153));
+        btnGuardar.setFont(new Font("Lucida Console", Font.PLAIN, 16));
         btnGuardar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 curso.setTitulo(txtTitulo.getText());
@@ -110,12 +130,12 @@ public class PantallaEditarCurso extends JFrame {
                 dispose();
             }
         });
-        btnGuardar.setBounds(104, 348, 180, 41);
+        btnGuardar.setBounds(403, 298, 271, 30);
         contentPane.add(btnGuardar);
 
         JButton btnCancelar = new JButton("Cancelar");
-        btnCancelar.setBackground(new Color(255, 255, 255));
-        btnCancelar.setFont(new Font("Leelawadee UI Semilight", Font.PLAIN, 16));
+        btnCancelar.setBackground(new Color(255, 255, 153));
+        btnCancelar.setFont(new Font("Lucida Console", Font.PLAIN, 16));
         btnCancelar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 PantallaMisCursos misCursos = new PantallaMisCursos(perfil);
@@ -123,8 +143,64 @@ public class PantallaEditarCurso extends JFrame {
                 dispose();
             }
         });
-        btnCancelar.setBounds(494, 348, 180, 41);
+        btnCancelar.setBounds(494, 347, 180, 30);
         contentPane.add(btnCancelar);
+        
+        JPanel panel_1_1_1 = new JPanel();
+        panel_1_1_1.setForeground(new Color(51, 51, 0));
+        panel_1_1_1.setBorder(null);
+        panel_1_1_1.setBackground(new Color(153, 0, 0));
+        panel_1_1_1.setBounds(-51, 20, 906, 44);
+        contentPane.add(panel_1_1_1);
+        
+        JLabel lblHorario = new JLabel("Horario:");
+        lblHorario.setForeground(Color.WHITE);
+        lblHorario.setFont(new Font("Lucida Console", Font.PLAIN, 12));
+        lblHorario.setBounds(104, 347, 100, 20);
+        contentPane.add(lblHorario);
+        
+        JLabel lblHhmm = new JLabel("hh/mm");
+        lblHhmm.setHorizontalAlignment(SwingConstants.CENTER);
+        lblHhmm.setForeground(new Color(255, 153, 153));
+        lblHhmm.setFont(new Font("Lucida Console", Font.PLAIN, 12));
+        lblHhmm.setBounds(84, 365, 100, 20);
+        contentPane.add(lblHhmm);
+        
+        JPanel panel_1_1_2_1 = new JPanel();
+        panel_1_1_2_1.setForeground(new Color(51, 51, 0));
+        panel_1_1_2_1.setBackground(new Color(255, 204, 0));
+        panel_1_1_2_1.setBounds(224, 100, 460, 35);
+        contentPane.add(panel_1_1_2_1);
+        
+        JPanel panel_1_1_2_1_1 = new JPanel();
+        panel_1_1_2_1_1.setForeground(new Color(51, 51, 0));
+        panel_1_1_2_1_1.setBackground(new Color(255, 204, 0));
+        panel_1_1_2_1_1.setBounds(224, 150, 460, 35);
+        contentPane.add(panel_1_1_2_1_1);
+        
+        JPanel panel_1_1_2_1_2 = new JPanel();
+        panel_1_1_2_1_2.setForeground(new Color(51, 51, 0));
+        panel_1_1_2_1_2.setBackground(new Color(255, 204, 0));
+        panel_1_1_2_1_2.setBounds(224, 202, 460, 35);
+        contentPane.add(panel_1_1_2_1_2);
+        
+        JPanel panel_1_1_2_1_3 = new JPanel();
+        panel_1_1_2_1_3.setForeground(new Color(51, 51, 0));
+        panel_1_1_2_1_3.setBackground(new Color(255, 204, 0));
+        panel_1_1_2_1_3.setBounds(274, 253, 410, 35);
+        contentPane.add(panel_1_1_2_1_3);
+        
+        JPanel panel_1_1_2_1_4 = new JPanel();
+        panel_1_1_2_1_4.setForeground(new Color(51, 51, 0));
+        panel_1_1_2_1_4.setBackground(new Color(255, 204, 0));
+        panel_1_1_2_1_4.setBounds(504, 356, 180, 30);
+        contentPane.add(panel_1_1_2_1_4);
+        
+        JPanel panel_1 = new JPanel();
+        panel_1.setForeground(new Color(51, 51, 0));
+        panel_1.setBackground(new Color(128, 0, 0));
+        panel_1.setBounds(413, 304, 271, 32);
+        contentPane.add(panel_1);
 
         setVisible(true);
     }
